98bda38aeff1ae6d2ad785c4a04c962e3df37d7f
Jeff Stanton <jstanton397@yahoo.com>
2016-01-03 22:37:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var main = function() {
   $('.flappy-project').click(function() {
     $('.project-detail').children().hide();
      $('.flappy-project-detail').animate({
-      'marginLeft': "100px"
+      'marginLeft': "-800px"
     });
     $('.flappy-project-detail').show(0);
  $('.flappy-project-detail').addClass('animated fadeInDown');
@@ -11,7 +11,7 @@ var main = function() {
     $('.calculator-project').click(function() {
       $('.project-detail').children().hide();
       $('.calculator-project-detail').animate({
-        'marginLeft': "500px"
+        'marginLeft': "0px"
       });
       $('.calculator-project-detail').show(0);
       $('.calculator-project-detail').addClass('animated fadeInDown');
@@ -20,7 +20,7 @@ var main = function() {
   $('.text-project').click(function() {
     $('.project-detail').children().hide();
     $('.text-project-detail').animate({
-      'marginLeft': "900px"
+      'marginLeft': "800px"
     });
     $('.text-project-detail').show(0);
     $('.text-project-detail').addClass('animated fadeInDown');
