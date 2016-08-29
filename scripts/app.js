28ccef79b9f82c6df14c184a40cc6bbcdafba773
Jeff Stanton <jstanton397@yahoo.com>
2016-08-29 01:30:05 -0700

    Added typing effect

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
+var caption = "Computer Science student from University of California Irvine. "
+var captionLength = 0;
 
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+    captionEl = $('#caption');
+    
+    type();
+});
+
+function type() {
+    captionEl.html(caption.substr(0, captionLength++));
+    if(captionLength < caption.length+1) {
+        setTimeout('type()', 50);
+    } else {
+        captionLength = 0;
+        caption = '';
+    }
+}
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
