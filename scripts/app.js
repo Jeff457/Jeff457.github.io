2654a0399ce5161d3a1f22d45fa2359944a3384c
Jeff Stanton <jstanton397@yahoo.com>
2016-08-31 02:15:48 -0700

    Animated menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,36 @@
 var caption = "Computer Science student from University of California Irvine. "
 var captionLength = 0;
 
-
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 600);
     captionEl = $('#caption');
-    
+    button = $('#toggle-menu');
     type();
-});
+  
+  $('#toggle-menu').click(function() {
+    
+    $('.menu').animate({
+      right: "0px"
+    }, 200);
+
+    $('body').animate({
+      right: "75px"
+    }, 100);
+    
+  });  // end open menu
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      right: "-285px"
+    }, 200);
+
+    $('body').animate({
+      right: "0px"
+    }, 200);
+  });  //end close menu
+  
+});  // end document ready
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
