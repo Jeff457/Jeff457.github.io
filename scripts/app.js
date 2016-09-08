11e9ab1f42957572bb6bea7b39d3d72d04f660b5
Jeff Stanton <jstanton397@yahoo.com>
2016-09-07 20:19:03 -0700

    added animations for sections

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,22 @@
 var caption = "Computer Science student from University of California Irvine. "
 var captionLength = 0;
 
+// cache reference to window and animation items
+var $animationLeft = $('.animation-left');
+var $animationRight = $('.animation-right');
+var $window = $(window);
+
+
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 600);
     captionEl = $('#caption');
     button = $('#toggle-menu');
-    type();
+    setTimeout( type, 2000);
+    $window.on('scroll resize', check_if_left_in_view);
+    $window.on('scroll resize', check_if_right_in_view);
+    $window.trigger('scroll');
   
+  // function animates the menu
   $('#toggle-menu').click(function() {
     
     $('.menu').animate({
@@ -32,6 +42,7 @@ $(document).ready(function() {
   
 });  // end document ready
 
+// function to animate introduction text
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
     if(captionLength < caption.length+1) {
@@ -40,12 +51,55 @@ function type() {
         captionLength = 0;
         caption = '';
     }
-}
+}  // end type
 
+// function to animate cursor in introduction text
 function cursorAnimation() {
     $('#cursor').animate({
         opacity: 0
     }, 'fast', 'swing').animate({
         opacity: 1
     }, 'fast', 'swing');
-}
+}  // end cursorAnimation
+
+function check_if_left_in_view() {
+  var windowHeight = $window.height();
+  var windowTopPosition = $window.scrollTop();
+  var windowBottomPosition = (windowTopPosition + windowHeight);
+  
+  $.each($animationLeft, function() {
+    var $element = $(this);
+    var elementHeight = $element.outerHeight();
+    var elementTopPosition = $element.offset().top;
+    var elementBottomPosition = (elementTopPosition + elementHeight);
+    
+    //check to see if this current container is within viewport
+    if ( (elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition) ) {
+      $element.addClass('animated fadeInLeft');
+    }
+    else {
+      $element.removeClass('animated fadeInLeft');
+    }
+  });
+}  // end check_if_skills_in_view
+
+function check_if_right_in_view() {
+  var windowHeight = $window.height();
+  var windowTopPosition = $window.scrollTop();
+  var windowBottomPosition = (windowTopPosition + windowHeight);
+  
+  $.each($animationRight, function() {
+    var $element = $(this);
+    var elementHeight = $element.outerHeight();
+    var elementTopPosition = $element.offset().top;
+    var elementBottomPosition = (elementTopPosition + elementHeight);
+    
+    //check to see if this current container is within viewport
+    if ( (elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition) ) {
+      $element.addClass('animated fadeInRight');
+    }
+    else {
+      $element.removeClass('animated fadeInRight');
+    }
+  });
+}  // end check_if_quotes_in_view
